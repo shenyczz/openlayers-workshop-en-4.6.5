e6ec40ffd05424ad65587d9f7644ffdf91567146
shenyc <shenyczz@163.com>
2020-09-18 19:16:39 +0800

    basics - Drawing a point at your location

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ import XYZSource from 'ol/source/xyz';
 //! [import-proj]
 import proj from 'ol/proj';
 
+//! [import-vector]
+import VectorLayer from 'ol/layer/vector';
+import VectorSource from 'ol/source/vector';
+import Feature from 'ol/feature';
+import Point from 'ol/geom/point';
+
+//! [import-style]
+import Style from 'ol/style/style';
+import IconStyle from 'ol/style/icon';
+
+
+
+
+
 //! [map-const]
 const map = new Map({
   target: 'map-container',
@@ -23,11 +37,25 @@ const map = new Map({
   })
 });
 
+//! [point-layer]
+const position = new VectorSource();
+const vector = new VectorLayer({
+  source: position
+});
+map.addLayer(vector);
+
+//! [style]
+vector.setStyle(new Style({
+  image: new IconStyle({
+    src: './data/marker.png'
+  })
+}));
+
 // Zooming to your location
 navigator.geolocation.getCurrentPosition(function(pos) {
   const coords = proj.fromLonLat([pos.coords.longitude, pos.coords.latitude]);
   map.getView().animate({ center: coords, zoom: 10 });
   //! [add-point]
-  //   position.addFeature(new Feature(new Point(coords)));
+  position.addFeature(new Feature(new Point(coords)));
   //! [add-point]
 });
