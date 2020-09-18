1caf218db241bfdd2bcf6cff0187e5d674449a7a
shenyc <shenyczz@163.com>
2020-09-18 19:06:11 +0800

    basics - Zooming to your location

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ import View from 'ol/view';
 import TileLayer from 'ol/layer/tile';
 import XYZSource from 'ol/source/xyz';
 
-new Map({
+//! [import-proj]
+import proj from 'ol/proj';
+
+//! [map-const]
+const map = new Map({
   target: 'map-container',
   layers: [
     new TileLayer({
@@ -17,4 +21,13 @@ new Map({
     center: [0, 0],
     zoom: 2
   })
+});
+
+// Zooming to your location
+navigator.geolocation.getCurrentPosition(function(pos) {
+  const coords = proj.fromLonLat([pos.coords.longitude, pos.coords.latitude]);
+  map.getView().animate({ center: coords, zoom: 10 });
+  //! [add-point]
+  //   position.addFeature(new Feature(new Point(coords)));
+  //! [add-point]
 });
