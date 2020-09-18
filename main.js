30a98bc994826fe5cfec8d38e884d3ebe6f41c33
shenyc <shenyczz@163.com>
2020-09-18 23:59:29 +0800

    VectorData - Rendering GeoJSON

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-alert("hello openlayer!");
+import 'ol/ol.css';
+import Map from 'ol/map';
+import View from 'ol/view';
+import GeoJSON from 'ol/format/geojson';
+import VectorLayer from 'ol/layer/vector';
+import VectorSource from 'ol/source/vector';
+
+const map = new Map({
+  target: 'map-container',
+  layers: [
+    new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: './data/countries.json'
+      })
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
